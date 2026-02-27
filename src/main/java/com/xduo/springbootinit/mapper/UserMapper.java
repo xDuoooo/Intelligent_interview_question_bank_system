5ea2dead17a453fd517d7e1379497fe09e87a57a
xduo <1257180096@qq.com>
2026-02-28 00:57:08 +0800

    init

---
+++ src/main/java/com/xduo/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.xduo.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xduo.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
